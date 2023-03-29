7b02fe6bc8f0bd684525955b5d3d0b9e112b4743
steve <emailstevekim@gmail.com>
2023-03-29 16:09:47 -0400

    update readme

--- dem_crop_persp3d.R
+++ dem_crop_persp3d.R
@@ -7,7 +7,7 @@ library("sf")
 db <- dbConnect(dbDriver("PostgreSQL"), user = 'steve', dbname = 'world')
 countries <- pgGetGeom(db, c("public", "ne_10m_admin_0_countries_lakes"), geom = "geom", gid = "fid", query=NULL)
 
-dem <- raster('topo15_4320.tif')
+dem <- raster('/home/steve/maps/srtm/topo15_4320.tif')
 dem <- setMinMax(dem)
 mask <- countries[countries$name == "Brazil",]
 dem <- mask(dem, mask)
@@ -25,7 +25,7 @@ expand = 0.005
 resfac = 0.1
 
 file = paste("topo_brazil_persp3d_", system('date +%Y%m%d%H%M%S', intern=TRUE) ,".png", sep="")
-png(file, bg='transparent')
-persp3D(x, y, z, bty='n', colkey=FALSE, resfac=resfac, expand=expand, scale=FALSE, facets=TRUE, curtain=FALSE, lighting=FALSE, smooth=FALSE, inttype=2, breaks=NULL, colvar=z, NAcol=NA, col=col(100), border='black', lwd=0.1, alpha=1, shade=0, lphi=-135, ltheta=0, add=FALSE, plot=TRUE)
-plotdev(theta=70, phi=70)
-dev.off()
+#png(file, bg='transparent')
+persp3D(x, y, z, bty='n', colkey=FALSE, resfac=resfac, expand=expand, scale=FALSE, facets=TRUE, curtain=FALSE, lighting=TRUE, smooth=FALSE, inttype=2, breaks=NULL, colvar=z, NAcol=NA, col=col(100), border='black', lwd=0.1, alpha=1, shade=1, lphi=0, ltheta=0, add=FALSE, plot=TRUE)
+plotdev(theta=90, phi=50)
+#dev.off()
